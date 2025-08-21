153a2f98276567d56b1306d8fee4e9113a35f3e2
Bhavin123A <bhavinadreja3980@gmail.com>
2025-08-21 15:50:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    num1 = int(request.form["num1"])
+    num2 = int(request.form["num2"])
+    result = num1 + num2
+    return render_template("result.html", num1=num1, num2=num2, result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
